7f9836dbc16d0079b3aaff51ea0cb06df36a3b35
Pragya Bharti <79440992+pragyag15@users.noreply.github.com>
2024-04-06 23:54:16 +0530

    Add files via upload

---
+++ inverttri.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+    int i,j,k;
+    for(i=0;i<=9;i++)
+    {
+        for(j=0;j<=1;j++)
+        {
+            std::cout<<"*";
+        }
+    }
+    std::cout<<std::endl;
+
+    for(i=0;i<=8;i++)
+    {
+        for(j=0;j<=1;j++)
+        {
+            std::cout<<"*";
+        }
+    }
+    std::cout<<std::endl;
+
+    for(i=0;i<=7;i++)
+    {
+        for(j=0;j<=1;j++)
+        {
+            std::cout<<"*";
+        }
+    }
+    std::cout<<std::endl;
+
+    for(i=0;i<=6;i++)
+    {
+        for(j=0;j<=1;j++)
+        {
+            std::cout<<"*";
+        }
+    }
+    std::cout<<std::endl;
+
+    for(i=0;i<=5;i++)
+    {
+        for(j=0;j<=1;j++)
+        {
+            std::cout<<"*";
+        }
+    }
+    std::cout<<std::endl;
+
+    for(i=0;i<=4;i++)
+    {
+        for(j=0;j<=1;j++)
+        {
+            std::cout<<"*";
+        }
+    }
+    std::cout<<std::endl;
+
+    for(i=0;i<=3;i++)
+    {
+        for(j=0;j<=1;j++)
+        {
+            std::cout<<"*";
+        }
+    }
+    std::cout<<std::endl;
+
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=1;j++)
+        {
+            std::cout<<"*";
+        }
+    }
+    std::cout<<std::endl;
+
+    for(i=0;i<=1;i++)
+    {
+        for(j=0;j<=1;j++)
+        {
+            std::cout<<"*";
+        }
+    }
+    std::cout<<std::endl;
+
+    for(i=0;i<=0;i++)
+    {
+        for(j=0;j<=0;j++)
+        {
+            std::cout<<"*";
+        }
+    }
+    std::cout<<std::endl;
+
+    for(i=0;i<=0;i++)
+    {
+        for(j=0;j<=0;j++)
+        {
+            std::cout<<"*";
+        }
+    }
+    getch();
+}
